14baf904ddd2a765dc604ff46237b433e9b84591
Geir-Ingi <geir.ingi@gmail.com>
2019-02-11 01:08:51 +0000

    functionality to SeasonDisplay and added loading screen functionality

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,42 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>;
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr it is cold',
+        iconName: 'snowflake'
+    }
+};
+
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9) {
+        if(lat > 0) {
+            return 'summer';
+        } else return 'winter';
+    } else {
+        if(lat > 0){
+            return 'winter';
+        } else return 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    
+    
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName} = seasonConfig[season];
+    console.log(season);
+    return(
+         <div className={`season-display ${season}`}>
+             <i className={`icon-left massive ${iconName} icon`} />
+             <h1>{text}</h1>
+             <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    )
 };
 
 export default SeasonDisplay;
